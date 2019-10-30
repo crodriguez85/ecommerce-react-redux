2cd44a556f401791859ada9cd185ba83f458a669
Cristian <cristian@addshop.app>
2019-10-30 16:38:58 -0300

    header

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Homepage from './pages/homepage/Homepage';
 import ShopPage from './pages/shop/shop';
+import Header from './components/header/Header';
 
 function App() {
   return (
     <Router>
+      <Header/>
       <Switch>
         <Route exact path='/' component={Homepage}/>
         <Route exact path='/shop' component={ShopPage}/>
