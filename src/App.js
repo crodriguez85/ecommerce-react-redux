881b7824f05e95ba0ddc749f19f06d2d14949183
Cristian <cristian@addshop.app>
2019-10-30 08:49:36 -0300

    HOC

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Homepage from './pages/homepage/Homepage';
 
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+)
+
 function App() {
   return (
-    <div className="App">
-      <Homepage/>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path='/' component={Homepage}/>
+        <Route exact path='/shop/hats' component={HatsPage}/>
+      </Switch>
+    </Router>
   );
 }
 
